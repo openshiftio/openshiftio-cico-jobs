babb57ddca912712aff81159ccff2ea978e2e723
Jaime Melis <jmelis@redhat.com>
2018-03-16 17:32:09 +0100

    Improve output of cico-jobs-jjb-test

--- cico_pr_test.sh
+++ cico_pr_test.sh
@@ -2,4 +2,12 @@
 
 set -ex
 
-jenkins-jobs test devtools-ci-index.yaml
+NEW_JOBS=$(mktemp -d)
+MASTER_JOBS=$(mktemp -d)
+
+jenkins-jobs test devtools-ci-index.yaml -o $NEW_JOBS
+
+git show master:devtools-ci-index.yaml | jenkins-jobs test -o $MASTER_JOBS
+
+diff -uNr $MASTER_JOBS $NEW_JOBS
+rm -rf $MASTER_JOBS $NEW_JOBS
