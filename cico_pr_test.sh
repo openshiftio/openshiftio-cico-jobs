6c5fafeec74d9471bae659de670fa462ae83ccbc
Jaime Melis <jmelis@redhat.com>
2018-03-16 17:53:39 +0100

    Make sure it always cleans the tmp directories

--- cico_pr_test.sh
+++ cico_pr_test.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/bash
 
-set -ex
+set -x
 
 NEW_JOBS=$(mktemp -d)
 MASTER_JOBS=$(mktemp -d)
 
+delete_tmp() {
+    rm -rf $NEW_JOBS $MASTER_JOBS
+}
+
 jenkins-jobs test devtools-ci-index.yaml -o $NEW_JOBS
+ret=$?
+
+if [ "$ret" != "0" ]; then
+    delete_tmp
+    exit $ret
+fi
 
 git show origin/master:devtools-ci-index.yaml | jenkins-jobs test -o $MASTER_JOBS
 
 diff -uNr $MASTER_JOBS $NEW_JOBS
-rm -rf $MASTER_JOBS $NEW_JOBS
+
+delete_tmp
+
+exit 0
