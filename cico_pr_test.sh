d50a8481c2b346e6921178a34ada222e88ff70f2
Jaime Melis <jmelis@redhat.com>
2018-03-16 17:44:42 +0100

    Master does not exist

--- cico_pr_test.sh
+++ cico_pr_test.sh
@@ -7,7 +7,7 @@ MASTER_JOBS=$(mktemp -d)
 
 jenkins-jobs test devtools-ci-index.yaml -o $NEW_JOBS
 
-git show master:devtools-ci-index.yaml | jenkins-jobs test -o $MASTER_JOBS
+git show origin/master:devtools-ci-index.yaml | jenkins-jobs test -o $MASTER_JOBS
 
 diff -uNr $MASTER_JOBS $NEW_JOBS
 rm -rf $MASTER_JOBS $NEW_JOBS
